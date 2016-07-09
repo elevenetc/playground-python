eaccc9d6bd0c2255c89ef5885b1c943fabed52fe
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-09 23:11:00 +0200

    Add gitignore and copy clipboard sample

--- copy-buffer/copy-buffer.py
+++ copy-buffer/copy-buffer.py
@@ -1,1 +1,9 @@
-print 'sss'
+import subprocess
+
+def getClipboardData():
+ p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+ retcode = p.wait()
+ data = p.stdout.read()
+ return data
+
+print getClipboardData()
